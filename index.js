6a246c44530bd9d45040518c1dc9bafdf6405680
Nabeil <nabeillemar@gmail.com>
2021-11-20 15:18:14 -0800

    commit tree 11e23b58bd1c971c93785a9330c3c0df92e6b2b6

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(strTime){
+  const time = parseInt(strTime, 10);
+  if ( time < 12) return "Good Morning"
+  if ( time > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  const content = document.getElementById('greeting')
+  content.textContent = message
+}
